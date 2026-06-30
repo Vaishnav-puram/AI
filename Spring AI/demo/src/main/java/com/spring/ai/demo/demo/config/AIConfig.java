ced91bf1bbaaf09355ee6f7d6f1e839e22d64e36
Vaishnav-puram <puramv307@gmail.com>
2026-06-30 22:39:21 +0530

    implemented chat memory (persistent memory) --> jdbc chat memory

--- Spring AI/demo/src/main/java/com/spring/ai/demo/demo/config/AIConfig.java
+++ Spring AI/demo/src/main/java/com/spring/ai/demo/demo/config/AIConfig.java
@@ -6,6 +6,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.ai.openai.OpenAiChatModel;
@@ -35,6 +37,15 @@ public class AIConfig {
                         .temperature(0.7)
                         .numPredict(512)
                         .build())
+                .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository){
+
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(10)
                 .build();
     }
 
